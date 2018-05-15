33d37f6e06cc3447cd236810055f9c3e8db04d85
Lippman, Ethan <elippman@hstnyc-rosen1.companynet.org>
2018-05-15 13:13:22 -0400

    Initial Commit

---
+++ XcodeGitTestTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeGitTestTwo
+//
+//  Created by Lippman, Ethan on 5/15/18.
+//  Copyright © 2018 Lippman, Ethan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
